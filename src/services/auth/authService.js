d70304ffaeb6e93482b025bbfbb4ef4e4cf60dc8
Uzer Jamal <ujamal@mindfiresolutions.com>
2025-07-14 11:13:10 +0530

    feat: login page with validations Add login page with yup form validations

--- src/services/auth/authService.js
+++ src/services/auth/authService.js
@@ -3,3 +3,5 @@ import axiosInstance from "../../lib/axios";
 
 export const register = (data) =>
   axiosInstance.post(API_ROUTES.AUTH.REGISTER, data);
+
+export const login = (data) => axiosInstance.post(API_ROUTES.AUTH.LOGIN, data);
